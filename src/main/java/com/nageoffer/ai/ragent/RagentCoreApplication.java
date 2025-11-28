e5c74aeff3bd94709e059bf8d1653bc14d10c741
ding.ma <machen@apache.org>
2025-11-28 23:14:57 +0800

    添加Ragent核心启动类注释

--- src/main/java/com/nageoffer/ai/ragent/RagentCoreApplication.java
+++ src/main/java/com/nageoffer/ai/ragent/RagentCoreApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Ragent 核心应用启动类
+ */
 @SpringBootApplication
 @MapperScan("com.nageoffer.ai.ragent.dao.mapper")
 public class RagentCoreApplication {
